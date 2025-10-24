e905e9995792c976dd6d660a44f411531dac0c47
Sergi <78795405+SeergiCureero@users.noreply.github.com>
2025-10-24 10:36:05 +0200

    Update KUKI_RP2040.ino

--- KUKI_RP2040.ino
+++ KUKI_RP2040.ino
@@ -4,47 +4,16 @@
 String msg;
 char dir;
 int vel;
-void setup() {
-  // Pin GPIO5 com a RX
-  // Pin GPIO4 com a TX
-  
-  Serial1.begin(9600); // UART1: RX=5, TX=4 
-  Serial.begin(9600); // Debug por puerto USB
-  while (!Serial);
-  // initialize the BLE hardware
-  BLE.begin();
-  Serial.println("KUKI");
-  // start scanning for Button Device BLE peripherals
-  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
-}
-void loop() {
-    
-  // Envia mensaje al MEGA
-  
 
-  // check if a peripheral has been discovered
-  BLEDevice peripheral = BLE.available();
-  if (peripheral) {
-    // discovered a peripheral, print out address, local name, and advertised service
-    Serial.print("Found ");
-    Serial.print(peripheral.address());
-    Serial.print(" '");
-    Serial.print(peripheral.localName());
-    Serial.print("' ");
-    Serial.print(peripheral.advertisedServiceUuid());
-    Serial.println();
-   
-    if (peripheral.localName().indexOf("Mando Kuki") < 0) {
-      Serial.println("Kuki no encontrado");
-      return;  // If the name doesn't have "Button Device" in it then ignore it
-    }
-    // stop scanning
-    BLE.stopScan();
-    prog(peripheral);
-    // peripheral disconnected, start scanning again
-    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
-  }
-}
+bool COMSBLT = false;
+
+char instrucciones[] = {'a','n','b','n','c','n','d','n','e','n','f','n','g','n','h','n'}; 
+int numeroInstruccion = 0;
+
+unsigned long tiempoActual = 0;       //cuando se alcance este tiempo se ejecuta cierta parte del codigo
+unsigned long tiempoAnterior = 0;     // Guardamos la ultima ejecucion
+const unsigned long intervalo = 100;  // 100 ms
+
 void prog(BLEDevice peripheral) {
   // connect to the peripheral
   Serial.println("Connecting ...");
@@ -84,7 +53,7 @@ void prog(BLEDevice peripheral) {
   }
   while (peripheral.connected()) {
     // while the peripheral is connected
-   if (X.canRead() && Y.canRead() &&  Vel.canRead()) {
+    if (X.canRead() && Y.canRead() &&  Vel.canRead()) {
       // Buffers para cada float (4 bytes)
       uint8_t bufX[4], bufY[4], bufVel[4];
 
@@ -123,9 +92,73 @@ void prog(BLEDevice peripheral) {
     Serial1.println(msg);
     Serial.print("Mensaje enviado: ");
     Serial.println(msg);
-  
+
     delay(100);
 
   }
   Serial.println("Peripheral disconnected");
 }
+
+
+void setup() {
+  // Pin GPIO5 com a RX
+  // Pin GPIO4 com a TX
+  
+  Serial1.begin(9600); // UART1: RX=5, TX=4 
+  Serial.begin(9600); // Debug por puerto USB
+  while (!Serial);
+  // initialize the BLE hardware
+  BLE.begin();
+  Serial.println("KUKI");
+  // start scanning for Button Device BLE peripherals
+  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+}
+
+void loop() {
+    
+  // Envia mensaje al MEGA
+  
+
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+  if (peripheral && COMSBLT) {
+    // discovered a peripheral, print out address, local name, and advertised service
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+   
+    if (peripheral.localName().indexOf("Mando Kuki") < 0) {
+      Serial.println("Kuki no encontrado");
+      return;  // If the name doesn't have "Button Device" in it then ignore it
+    }
+    // stop scanning
+    BLE.stopScan();
+    
+    prog(peripheral);
+    
+    
+    // peripheral disconnected, start scanning again
+    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+  }
+  else{
+    //automatico, sin comunicacion
+    vel = 50;
+    tiempoActual = millis();
+
+    if (tiempoActual - tiempoAnterior >= intervalo) {
+      tiempoAnterior = tiempoActual;  // Actualiza el contador
+      if(numeroInstruccion < sizeof(instrucciones)){
+        numeroInstruccion += 1;
+      }
+    }
+    msg = instrucciones[numeroInstruccion] + String(vel);
+  }
+}
+
+
+
+
